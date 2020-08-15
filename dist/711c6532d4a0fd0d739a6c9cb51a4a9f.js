d3c89a1db58fae0f2a386bf82e1769bae661aa24
Glitch (smartquiz-me) <none>
2020-08-15 13:56:17 +0000

    🚧🍙 parcel

---
+++ dist/711c6532d4a0fd0d739a6c9cb51a4a9f.js
@@ -0,0 +1,538 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+require = (function (modules, cache, entry) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof require === "function" && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof require === "function" && require;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({10:[function(require,module,exports) {
+/* Template: Tivo - SaaS App HTML Landing Page Template
+   Author: Inovatik
+   Created: Sep 2019
+   Description: Custom JS file
+*/
+
+(function ($) {
+    "use strict";
+
+    /* Preloader */
+
+    $(window).on('load', function () {
+        var preloaderFadeOutTime = 500;
+        function hidePreloader() {
+            var preloader = $('.spinner-wrapper');
+            setTimeout(function () {
+                preloader.fadeOut(preloaderFadeOutTime);
+            }, 500);
+        }
+        hidePreloader();
+    });
+
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function () {
+        if ($(".navbar").offset().top > 60) {
+            $(".fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function () {
+        $(document).on('click', 'a.page-scroll', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 600, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    // closes the responsive menu on menu item click
+    $(".navbar-nav li a").on("click", function (event) {
+        if (!$(this).parent().hasClass('dropdown')) $(".navbar-collapse").collapse('hide');
+    });
+
+    /* Video Lightbox - Magnific Popup */
+    $('.popup-youtube, .popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+        iframe: {
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: function id(url) {
+                        var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+                        if (!m || !m[1]) return null;
+                        return m[1];
+                    },
+                    src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+                },
+                vimeo: {
+                    index: 'vimeo.com/',
+                    id: function id(url) {
+                        var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+                        if (!m || !m[5]) return null;
+                        return m[5];
+                    },
+                    src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+                }
+            }
+        }
+    });
+
+    /* Details Lightbox - Magnific Popup */
+    $('.popup-with-move-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    });
+
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function () {
+        if ($(this).val() != '') {
+            $(this).addClass('notEmpty');
+        } else {
+            $(this).removeClass('notEmpty');
+        }
+    });
+
+    /* Sign Up Form */
+    $("#signUpForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            sformError();
+            ssubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            ssubmitForm();
+        }
+    });
+
+    function ssubmitForm() {
+        // initiate variables with form content
+        var email = $("#semail").val();
+        var name = $("#sname").val();
+        var password = $("#spassword").val();
+        var terms = $("#sterms").val();
+
+        $.ajax({
+            type: "POST",
+            url: "php/signupform-process.php",
+            data: "email=" + email + "&name=" + name + "&password=" + password + "&terms=" + terms,
+            success: function success(text) {
+                if (text == "success") {
+                    sformSuccess();
+                } else {
+                    sformError();
+                    ssubmitMSG(false, text);
+                }
+            }
+        });
+    }
+
+    function sformSuccess() {
+        $("#signUpForm")[0].reset();
+        ssubmitMSG(true, "Sign Up Submitted!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+    }
+
+    function sformError() {
+        $("#signUpForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass();
+        });
+    }
+
+    function ssubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#smsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+    /* Log In Form */
+    $("#logInForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            lformError();
+            lsubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            lsubmitForm();
+        }
+    });
+
+    function lsubmitForm() {
+        // initiate variables with form content
+        var email = $("#lemail").val();
+        var password = $("#lpassword").val();
+
+        $.ajax({
+            type: "POST",
+            url: "php/loginform-process.php",
+            data: "email=" + email + "&password=" + password,
+            success: function success(text) {
+                if (text == "success") {
+                    lformSuccess();
+                } else {
+                    lformError();
+                    lsubmitMSG(false, text);
+                }
+            }
+        });
+    }
+
+    function lformSuccess() {
+        $("#logInForm")[0].reset();
+        lsubmitMSG(true, "Log In Submitted!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+    }
+
+    function lformError() {
+        $("#logInForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass();
+        });
+    }
+
+    function lsubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#lmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+    /* Newsletter Form */
+    $("#newsletterForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            nformError();
+            nsubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            nsubmitForm();
+        }
+    });
+
+    function nsubmitForm() {
+        // initiate variables with form content
+        var email = $("#nemail").val();
+        var terms = $("#nterms").val();
+        $.ajax({
+            type: "POST",
+            url: "php/newsletterform-process.php",
+            data: "email=" + email + "&terms=" + terms,
+            success: function success(text) {
+                if (text == "success") {
+                    nformSuccess();
+                } else {
+                    nformError();
+                    nsubmitMSG(false, text);
+                }
+            }
+        });
+    }
+
+    function nformSuccess() {
+        $("#newsletterForm")[0].reset();
+        nsubmitMSG(true, "Subscribed!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+    }
+
+    function nformError() {
+        $("#newsletterForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass();
+        });
+    }
+
+    function nsubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#nmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+    /* Privacy Form */
+    $("#privacyForm").validator().on("submit", function (event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            pformError();
+            psubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            psubmitForm();
+        }
+    });
+
+    function psubmitForm() {
+        // initiate variables with form content
+        var name = $("#pname").val();
+        var email = $("#pemail").val();
+        var select = $("#pselect").val();
+        var terms = $("#pterms").val();
+
+        $.ajax({
+            type: "POST",
+            url: "php/privacyform-process.php",
+            data: "name=" + name + "&email=" + email + "&select=" + select + "&terms=" + terms,
+            success: function success(text) {
+                if (text == "success") {
+                    pformSuccess();
+                } else {
+                    pformError();
+                    psubmitMSG(false, text);
+                }
+            }
+        });
+    }
+
+    function pformSuccess() {
+        $("#privacyForm")[0].reset();
+        psubmitMSG(true, "Request Submitted!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+    }
+
+    function pformError() {
+        $("#privacyForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass();
+        });
+    }
+
+    function psubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#pmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });
+
+    /* Removes Long Focus On Buttons */
+    $(".button, a, button").mouseup(function () {
+        $(this).blur();
+    });
+})(jQuery);
+},{}],79:[function(require,module,exports) {
+
+var global = (1, eval)('this');
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    accept: function (fn) {
+      this._acceptCallback = fn || function () {};
+    },
+    dispose: function (fn) {
+      this._disposeCallback = fn;
+    }
+  };
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '41557' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      data.assets.forEach(function (asset) {
+        hmrApply(global.require, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.require, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + 'data.error.stack');
+    }
+  };
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(+k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  if (cached && cached.hot._disposeCallback) {
+    cached.hot._disposeCallback();
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallback) {
+    cached.hot._acceptCallback();
+    return true;
+  }
+
+  return getParents(global.require, id).some(function (id) {
+    return hmrAccept(global.require, id);
+  });
+}
+},{}]},{},[79,10])
+//# sourceMappingURL=/dist/711c6532d4a0fd0d739a6c9cb51a4a9f.map
